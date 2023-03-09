cef22609d85f66e1904102562e43be2b27c8b352
Asylkhan Suleymenov <asik89514003093@gmail.com>
2023-03-10 01:17:39 +0300

    add main

---
+++ code/main.py
@@ -0,0 +1,15 @@
+from code.utils import Data, ReformattedData, main
+
+PATH = 'data.json'
+
+data = Data(PATH)
+text = data.get_data()
+
+new_text = ReformattedData(text)
+reformatted_text = new_text.reformat_data()
+
+final_output = main(reformatted_text)[:5]
+
+for operation in final_output:
+    print(operation)
+    print()
